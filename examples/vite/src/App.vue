bb6ed6e8ab0de1ce97f83adaf2a395bdcf3ffabf
Rupert Angermeier <rupert.angermeier@gmail.com>
2024-07-08 10:11:49 +0200

    fix and simplify examples/vite
    - set selected item ref
    - remove random number from suggestions
    - remove vite logo and HelloWorld.vue

--- examples/vite/src/App.vue
+++ examples/vite/src/App.vue
@@ -1,27 +1,137 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <div class="demo">
+    <div 
+      v-if="selected" 
+    >
+      You have selected <code>{{ selected.name }}, the {{ selected.race }}</code>
+    </div>
+    <div class="autosuggest-container">
+      <vue-autosuggest
+        v-model="query"
+        v-slot="{suggestion}"
+        :suggestions="filteredOptions"
+        :get-suggestion-value="getSuggestionValue"
+        :input-props="{id:'autosuggest__input', placeholder:'Do you feel lucky, punk?'}"
+        @focus="focusMe"
+        @click="clickHandler"
+        @selected="onSelected"
+      >
+        <div 
+          style="display: flex; align-items: center;"
+        >
+          <img 
+            :style="{ display: 'flex', width: '25px', height: '25px', borderRadius: '15px', marginRight: '10px'}" 
+            :src="suggestion.item.avatar"
+          >
+          <div>{{ suggestion.item.name }}</div>
+        </div>
+      </vue-autosuggest>
+    </div>
+  </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import { ref, computed } from 'vue'
+import { VueAutosuggest } from "../../../src/vue-autosuggest";
 
-export default defineComponent({
-  name: 'App',
+export default {
   components: {
-    HelloWorld
+    VueAutosuggest
+  },
+  setup () {
+    const query = ref("")
+    const selected = ref("")
+    const suggestions = ref([
+      {
+        data: [
+          { id: 1, name: "Frodo", race: "Hobbit", avatar: "https://upload.wikimedia.org/wikipedia/en/thumb/4/4e/Elijah_Wood_as_Frodo_Baggins.png/220px-Elijah_Wood_as_Frodo_Baggins.png" },
+          { id: 2, name: "Samwise", race: "Hobbit", avatar: "https://upload.wikimedia.org/wikipedia/en/thumb/7/7b/Sean_Astin_as_Samwise_Gamgee.png/200px-Sean_Astin_as_Samwise_Gamgee.png" },
+          { id: 3, name: "Gandalf", race: "Maia", avatar: "https://upload.wikimedia.org/wikipedia/en/thumb/e/e9/Gandalf600ppx.jpg/220px-Gandalf600ppx.jpg" },
+          { id: 4, name: "Aragorn", race: "Human", avatar: "https://upload.wikimedia.org/wikipedia/en/thumb/3/35/Aragorn300ppx.png/150px-Aragorn300ppx.png" }
+        ]
+      }
+    ])
+  
+    const filteredOptions = computed(() => {
+        const filteredData = suggestions.value[0].data.filter(option => {
+              return option.name.toLowerCase().indexOf(query.value.toLowerCase()) > -1;
+            })
+        console.log('filteredOptions', filteredData)
+
+        return [
+          { 
+            data: filteredData
+          }
+        ]
+    })
+  
+    function clickHandler(item) {
+      // event fired when clicking on the input
+    }
+    
+    function onSelected(item) {
+      selected.value = item.item;
+    }
+  
+    function getSuggestionValue(suggestion) {
+      return suggestion.item.name;
+    }
+
+    function focusMe(e) {
+      console.log(e) // FocusEvent
+    }
+
+    return {
+      query,
+      selected,
+      suggestions,
+      filteredOptions,
+
+      clickHandler,
+      onSelected,
+      getSuggestionValue,
+      focusMe
+    }
   }
-})
+}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.demo { 
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+
+input {
+  width: 260px;
+  padding: 0.5rem;
+}
+
+ul {
+  width: 100%;
+  color: rgba(30, 39, 46,1.0);
+  list-style: none;
+  margin: 0;
+  padding: 0.5rem 0 .5rem 0;
+}
+li {
+  margin: 0 0 0 0;
+  border-radius: 5px;
+  padding: 0.75rem 0 0.75rem 0.75rem;
+  display: flex;
+  align-items: center;
+}
+li:hover {
+  cursor: pointer;
+}
+
+.autosuggest-container {
+  display: flex;
+  justify-content: center;
+  width: 280px;
+}
+
+#autosuggest { width: 100%; display: block;}
+.autosuggest__results-item--highlighted {
+  background-color: rgba(51, 217, 178,0.2);
 }
 </style>
